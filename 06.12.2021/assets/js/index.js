0d3419bb2831c8f494e4fcfc9c48c98985f8b541
eemaharramli <91149686+eemaharramli@users.noreply.github.com>
2021-12-07 15:35:55 +0400

    Update index.js

--- 06.12.2021/assets/js/index.js
+++ 06.12.2021/assets/js/index.js
@@ -17,9 +17,11 @@ function MultiplyOfNumbers(number1, number2) {
     return number1 * number2
 }
 
-
-alert(`Sum of numbers: ${SumOfNumbers(number1, number2)}`)
-alert(`Divide of numbers: ${DivideOfNumbers(number1, number2)}`)
-alert(`Difference of numbers: ${DifferenceOfNumbers(number1, number2)}`)
-alert(`Multiply of numbers: ${MultiplyOfNumbers(number1, number2)}`)
-
+if((number1 == parseInt(number1)) && (number2 == parseInt(number2)) ){
+	alert(`Sum of input numbers: ${SumOfNumbers(number1, number2)}`)
+	alert(`Difference of input numbers: ${DifferenceOfNumbers(number1, number2)}`)
+	alert(`Multiple of input numbers: ${MultiplyOfNumbers(number1, number2)}`)
+	alert(`Divide of input numbers: ${DivideOfNumbers(number1, number2)}`)
+}else{
+	alert('Some of inputs isn\'t a number')
+}
