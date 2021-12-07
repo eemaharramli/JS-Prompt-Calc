2a77e7a396353ba68ba1b1728ce72804e79e80b3
eemaharramli <91149686+eemaharramli@users.noreply.github.com>
2021-12-07 15:52:33 +0400

    Update index.js

--- 06.12.2021/assets/js/index.js
+++ 06.12.2021/assets/js/index.js
@@ -1,27 +1,35 @@
-let firstNumber = +prompt('Input first number')
-let secondNumber = +prompt('Input second number')
+let continution = false
+do{
+	let firstNumber = +prompt('Input first number')
+	let secondNumber = +prompt('Input second number')
 
-function SumOfNumbers(firstNumber, secondNumber) {
-    return firstNumber + secondNumber
-}
+	function SumOfNumbers(firstNumber, secondNumber) {
+		return firstNumber + secondNumber
+	}
 
-function DivideOfNumbers(firstNumber, secondNumber){
-    return firstNumber / secondNumber
-}
+	function DivideOfNumbers(firstNumber, secondNumber){
+		return firstNumber / secondNumber
+	}
 
-function DifferenceOfNumbers(firstNumber, secondNumber){
-    return firstNumber - secondNumber
-}
+	function DifferenceOfNumbers(firstNumber, secondNumber){
+		return firstNumber - secondNumber
+	}
 
-function MultiplyOfNumbers(firstNumber, secondNumber) {
-    return firstNumber * secondNumber
-}
+	function MultiplyOfNumbers(firstNumber, secondNumber) {
+		return firstNumber * secondNumber
+	}
 
-if((firstNumber == parseInt(firstNumber)) && (secondNumber == parseInt(secondNumber)) ){
-	alert(`Sum of input numbers: ${SumOfNumbers(number1, number2)}`)
-	alert(`Difference of input numbers: ${DifferenceOfNumbers(number1, number2)}`)
-	alert(`Multiple of input numbers: ${MultiplyOfNumbers(number1, number2)}`)
-	alert(`Divide of input numbers: ${DivideOfNumbers(number1, number2)}`)
-}else{
-	alert('Some of inputs isn\'t a number')
-}
+	if((firstNumber == parseInt(firstNumber)) && (secondNumber == parseInt(secondNumber)) ){
+		alert(`Sum of input numbers: ${SumOfNumbers(firstNumber, secondNumber)}`)
+		alert(`Difference of input numbers: ${DifferenceOfNumbers(firstNumber, secondNumber)}`)
+		alert(`Multiple of input numbers: ${MultiplyOfNumbers(firstNumber, secondNumber)}`)
+		alert(`Divide of input numbers: ${DivideOfNumbers(firstNumber, secondNumber)}`)
+	}else{
+		alert('Some of inputs isn\'t a number')
+	}
+	
+	let answer = confirm('Do you want to calculate more?')
+	
+	answer ? continution = true : continution = false
+	
+}while(continution)
